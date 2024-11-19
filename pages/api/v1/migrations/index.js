4f9dde6d8fe2db21f821c2caf74075fe1827e113
EUVALDO BARBOSA FERREIRA <33738067+euvaldoferreira@users.noreply.github.com>
2024-11-19 19:53:51 +0000

    .

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -12,21 +12,23 @@ export default async function migrations(request, response) {
     verbose: true,
     migrationsTable: "pgmigrations",
   };
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner(defaultMigrationOptions);
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
     await dbClient.end();
+    await response.status(200).json(pendingMigrations);
+  }
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+
     if (migratedMigrations.length > 0) {
       response.status(201).json(migratedMigrations);
     }
     response.status(200).json(migratedMigrations);
   }
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await response.status(200).json(pendingMigrations);
-  }
-  await dbClient.end();
+
   return response.status(405).end();
 }

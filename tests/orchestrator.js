a7501a852bff4b668a179076e6063c9ed3a1d600
EUVALDO BARBOSA FERREIRA <33738067+euvaldoferreira@users.noreply.github.com>
2024-11-27 12:56:27 +0000

    local merge

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,23 +5,11 @@ async function waitForAllServices() {
   async function awaitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
-<<<<<<< HEAD
-=======
-      maxTimeout: 1000,
->>>>>>> 0a637452adfaab2f32dafe63533854d5ae90b725
     });
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-<<<<<<< HEAD
       const responseBody = await response.json();
-=======
-      if (response.status === 200) {
-        return;
-      } else {
-        throw new Error("Web server not ready yet");
-      }
->>>>>>> 0a637452adfaab2f32dafe63533854d5ae90b725
     }
   }
 }

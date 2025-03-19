eccaf075506ea378b2a226eae4452c36432d6d9e
EUVALDO BARBOSA FERREIRA <33738067+euvaldoferreira@users.noreply.github.com>
2025-03-19 21:06:56 +0000

    feat: create and user `ValidatinError` in `controller.js`

---
+++ infra/errors.js
@@ -0,0 +1,73 @@
+export class InternalServerError extends Error {
+  constructor({ cause, statusCode }) {
+    super("An unexpected internal error occurred.", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Please try again later or contact support.";
+    this.statusCode = statusCode || 500;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Services currently unavailable.", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Check if the service is available.";
+    this.statusCode = 503;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method not allowed for this endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action = "Contact Support.";
+    this.statusCode = 405;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "A validation error was identified.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Please try again with valid data.";
+    this.statusCode = 400;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
